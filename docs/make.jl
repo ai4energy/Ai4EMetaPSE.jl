a7dd8fdeeada4d20f14510fc75c7aa74e4b352dd
jake484 <522432938@qq.com>
2022-09-19 16:54:42 +0800

    update url

--- docs/make.jl
+++ docs/make.jl
@@ -5,12 +5,12 @@ DocMeta.setdocmeta!(Ai4EMetaPSE, :DocTestSetup, :(using Ai4EMetaPSE); recursive=
 
 makedocs(;
     modules=[Ai4EMetaPSE],
-    authors="jake484 <522432938@qq.com> and contributors",
-    repo="https://github.com/jake484/Ai4EMetaPSE.jl/blob/{commit}{path}#{line}",
+    authors="ai4energy <522432938@qq.com> and contributors",
+    repo="https://github.com/ai4energy/Ai4EMetaPSE.jl/blob/{commit}{path}#{line}",
     sitename="Ai4EMetaPSE.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://jake484.github.io/Ai4EMetaPSE.jl",
+        canonical="https://ai4energy.github.io/Ai4EMetaPSE.jl",
         edit_link="main",
         assets=String[],
     ),
@@ -20,6 +20,6 @@ makedocs(;
 )
 
 deploydocs(;
-    repo="github.com/jake484/Ai4EMetaPSE.jl",
+    repo="github.com/ai4energy/Ai4EMetaPSE.jl",
     devbranch="main",
 )
